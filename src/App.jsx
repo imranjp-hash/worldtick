4fb0a9417f35eff22f534f3ffc54de3c558ed181
Jason P <imranjp@gmail.com>
2026-05-09 17:13:30 -0400

    restore app styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import "./App.css";
 const cities = [
   { name: "Toronto", country: "Canada", timeZone: "America/Toronto" },
   { name: "New York", country: "United States", timeZone: "America/New_York" },
